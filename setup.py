82abbefec54da24ecfd5df3c821f3c84cca74af7
henryjs <87094102+henryjs@users.noreply.github.com>
2023-05-23 23:12:10 +0100

    Fixed typo in scikit-image package in setup.py; replaced distutils with setuptools to use find_packages for sub-package detection.

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(
     name='MyoBand',
-    version='1.0',
+    packages=find_packages(),
+    version='1.3',
     url='https://github.com/hjsheehy/MyoBand',
     author="Henry Joseph Sheehy",
     author_email='henryjsheehy@gmail.com',
-    packages=['myoband',],
     license=open('LICENSE').read(),
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
-    install_requires=['numpy','scipy','matplotlib','mycolorpy','shapely','skikit-image'],
+    install_requires=['numpy','scipy','matplotlib','mycolorpy','shapely','scikit-image'],
 )
